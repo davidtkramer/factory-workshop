c8033ce5ed22b9617381d35c0cf6f174015fa896
David Kramer <david.kramer@redtailtechnology.com>
2019-11-07 14:24:59 -0800

    wip

--- test/factory_test.rb
+++ test/factory_test.rb
@@ -4,17 +4,23 @@ class Bicycle
   attr_accessor :style, :size, :color, :weight, :gears
 end
 
-Factory.define :bicycle, class: Bicycle do
-  add_attribute :color, :red
-  add_attribute :gears, 11
-end
-
 describe Factory do
-  describe 'run' do
-    it 'builds an instance of bicycle' do
-      bicycle = Factory.run(:bicycle)
-      assert_equal :red, bicycle.color
-      assert_equal 11, bicycle.gears
+  before do
+    Factory.define :bicycle, class: Bicycle do
+      add_attribute :color, :red
+      add_attribute :gears, 11
+      add_attribute :style, :road
+      add_attribute :weight, 20
+      add_attribute :size, :medium
     end
+  end
+
+  it 'builds an instance of bicycle' do
+    bicycle = Factory.run(:bicycle)
+    assert_equal :red, bicycle.color
+    assert_equal 11, bicycle.gears
+    assert_equal :road, bicycle.style
+    assert_equal 20, bicycle.weight
+    assert_equal :medium, bicycle.size
   end
 end
